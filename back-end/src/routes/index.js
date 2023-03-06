e4fe7c97b51a316ebedc23279cb53baa59fbf8c0
Emílio <emiliogabriel000@gmail.com>
2023-03-06 16:17:56 -0300

    Adicionando rota para procurar usuário pelo nick

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,11 +1,12 @@
-import {create, getUsers} from "../controllers/user-controllers"
+import {create, getUsers,getUser} from "../controllers/user-controllers"
 import { authenticate } from "../controllers/auth-controller";
 const authMid = require('../middlewares/auth')
 
 const userRoutes = app => {
     app.post("/register", create),
     app.post("/authenticate", authenticate),
-    app.get("/users", authMid,getUsers)
+    app.get("/users", authMid,getUsers),
+    app.get("/findUser", authMid, getUser)
 }
 
 export default userRoutes;
