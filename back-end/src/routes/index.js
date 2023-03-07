3d77276bf2a0693f5e7bca3140eaac9af4ea8b86
Emílio <emiliogabriel000@gmail.com>
2023-03-06 23:05:50 -0300

    Rota para cancelar a função seguindo

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,4 @@
-import {create, getUsers,getUser,followUser, followsUser} from "../controllers/user-controllers"
+import {create, getUsers,getUser,followUser, followsUser,unfollowUser} from "../controllers/user-controllers"
 import { authenticate } from "../controllers/auth-controller";
 const authMid = require('../middlewares/auth')
 
@@ -8,7 +8,8 @@ const userRoutes = app => {
     app.get("/users", authMid,getUsers),
     app.get("/findUser", authMid, getUser),
     app.post("/followUser", followUser),
-    app.post("followsUser", followsUser)
+    app.post("/followsUser", followsUser),
+    app.delete("/unfollowUser",unfollowUser)
 
 }
 
