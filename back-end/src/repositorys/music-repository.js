0fd2328a4da55fb3ee9fc4d3380792ab73966db4
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-03-19 19:51:51 -0300

    Upload funcionando da forma correta agora

--- back-end/src/repositorys/music-repository.js
+++ back-end/src/repositorys/music-repository.js
@@ -1,8 +1,17 @@
-import { prisma } from "../lib/prisma"
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const importMusic = async (userId, fileName) => {
+  try {
+    await prisma.$connect(); // garante que a conexão com o banco de dados está estabelecida
     const music = await prisma.music.create({
-        data: {userId, fileName}
+      data: { userId, fileName },
     });
     return music;
-}
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect(); // encerra a conexão com o banco de dados
+  }
+};
