7a711226294875f09eca4f55c61d1de81bd80066
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-03-19 23:46:36 -0300

    modificação no upload da musica em rotas

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -11,7 +11,7 @@ const userRoutes = app => {
   app.get("/users", authMid, getUsers);
   app.get("/music/:musicId", musicController.streamMusic);
   app.get("/music/user/:userId", authMid, musicController.getMusicByUser);
-  app.post("/upload", uploadMid, async (req, res) => {
+  app.post("/upload", uploadMid.single('file'), async (req, res) => {
     await importMusic(req.query.userId, req.file.filename, req.file.originalname, req.file.path);
     res.send("Arquivo Recebido com sucesso");
   });
