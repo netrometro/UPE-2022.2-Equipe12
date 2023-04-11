9ad1e60537f1a68ad69e38da2bc8e1fd3d4fc912
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-04-10 22:11:13 -0300

    work in progress

--- back-end/src/controllers/get-audio.js
+++ back-end/src/controllers/get-audio.js
@@ -6,10 +6,6 @@ const getAudio = async (req, res, next) => {
 
     const userId = req.userId;
 
-    if (!userId) {
-      return res.status(401).json({ success: false, message: "Usuário não autenticado" });
-    }
-
     const userMusics = await prisma.music.findMany({ where: { userId } });
     const userMusicIds = userMusics.map((music) => music.asset_id);
 
@@ -22,17 +18,15 @@ const getAudio = async (req, res, next) => {
       tags: "audio",
       max_results: 500,
     });
-    console.log(options)
 
     // Executa a busca
     const userAudios = options.resources.filter((audio) =>
-    userMusicIds.includes(String(audio.asset_id))
+    userMusicIds.includes(String(audio.asset_id, audio.filename))
   );
 
     // Retorna os resultados
     res.status(200).json({ success: true, data: userAudios, message: "Busca por arquivos de áudio realizada com sucesso"  });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ success: false, message: "Erro ao buscar arquivos de áudio" });
   }
 };
