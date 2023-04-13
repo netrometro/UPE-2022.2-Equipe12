2799673e78bfd6c57866920de745ef73e04c66a7
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-04-13 20:42:49 -0300

    pequenas alterações

--- front-end/upemusic/src/components/Footer/index.jsx
+++ front-end/upemusic/src/components/Footer/index.jsx
@@ -5,11 +5,18 @@ import '../Footer/styles.css';
 export const Footer = () => {
     return (
         <div className="footer">
-            <span className="name">Emílio Gabriel</span>
-
-            <a href="https://www.instagram.com/emilio.gabriel_"><FaInstagram className="icon" /></a>
-            <a href="https://www.linkedin.com/in/emiliogabriel18"><FaLinkedin className="icon" /></a>
-            <a href="https://github.com/Emio-eng"><FaGithub className="icon" /></a>
+            <div>
+                <span className="name">Emílio Gabriel</span>
+                <a href="https://www.instagram.com/emilio.gabriel_"><FaInstagram className="icon" /></a>
+                <a href="https://www.linkedin.com/in/emiliogabriel18"><FaLinkedin className="icon" /></a>
+                <a href="https://github.com/Emio-eng"><FaGithub className="icon" /></a>
+            </div>
+            <div>
+                <span className="name">Jonas Soares</span>
+                <a href="https://instagram.com/jonas_3301"><FaInstagram className="icon" /></a>
+                <a href="https://www.linkedin.com/in/jonas-soares-7a83391b8/"><FaLinkedin className="icon" /></a>
+                <a href="https://github.com/Jonah21-prog"><FaGithub className="icon" /></a>
+            </div>
         </div>
     );
 }
